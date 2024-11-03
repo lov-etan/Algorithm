d052ef51e82b3d9fa01d614ae1e270fb3cc2a624
Jiwoo <emily1290@naver.com>
2024-11-03 09:33:08 +0900

    [Silver II] Title: 나무 자르기, Time: 1012 ms, Memory: 119344 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,69 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
     static int N, M;
     static int[] trees;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        N = sc.nextInt(); //나무의 수
-        M = sc.nextInt(); //상근이가 집으로 가져가려고 하는 나무의 길이
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
         trees = new int[N];
-
+        st = new StringTokenizer(br.readLine());
         for(int i=0; i<N; i++){
-            trees[i] = sc.nextInt();
+            trees[i] = Integer.parseInt(st.nextToken());
         }
-
         Arrays.sort(trees);
-        //System.out.println(Arrays.toString(trees));
 
-
-        //이분탐색 //mid = 절단기 높이
-        int left = 0; //**필요한 길이가 M이면 절단기의 높이가 M이어도 됨
+        // 이분탐색 - 절단기 높이의 범위(0~나무의 최대 높이)를 설정하여 이 범위 안에서 이분탐색
+        int left = 0;
         int right = trees[N-1];
         int answer = 0;
 
-        //System.out.println(left + " " + right);
-
-
-        while(left<right){
-            int mid = (left+right)/2;
-            //System.out.println("left" + left + " " + "right" + right +  " " + "mid" + mid);
-            //System.out.println(cutting(mid));
-
-            long sum = cutting(mid);
-
-            if(sum >= M){ //자른 후의 결과가 M보다 크다면 -> 기준을 올려야 함
+        while(left < right){ //*** 공유기설치 문제와 비교해서 궁금한 점 (근사치도 가능할 때와 정확한 값을 원할 때의 범위 설정이 달라도 되는 것인가?
+            int mid = (left + right)/2;
+            long sum = cutting(mid); //mid 길이를 절단기의 높이로 가져가서 있는 나무들을 잘라보자. 얼마만큼의 길이가 모아지는가??
+                                        //M이 int형의 최댓값을 건들인다. 위험! long으로 튀자
+            if(sum >= M) { //원하는 길이보다 더 잘랐어 -> 기준을 올려야 해
                 answer = mid;
-                left = mid+1;
-            } else {
+                left = mid + 1;
+            } else { //원하는 길이보다 덜 잘랐어? -> 기준을 내려야 해
                 right = mid;
             }
-//            } else if(sum == M){ //** 나무를 항상 딱 맞게 가져갈 순 없다.
-//                answer = mid;
-//                break;
-//            }
-
-
-            //내가 원하는 양을 얻을 수 없는 조건일 때, 무한 루프가 생긴다.
-            //나무 높이가 다 10인데 우리는 11을 원해 - 11을 딱 맞춰는 불가능 - 그럼 6+6 12라도 가져가야 해
-
         }
-        System.out.println(answer);
 
+        System.out.println(answer);
     }
 
-    static long cutting(int cPoint){ //**나무의 길이 M이 2,000,000,000까지 가능하므로 이 값이 int형(2,147,483,647)을 넘길 가능성이 있다.
+    private static long cutting(int mid) {
         long sum = 0;
-        for(int i=0; i<N; i++){
-            if(trees[i] > cPoint){
-                sum += trees[i] - cPoint;
+        for(int treeheight : trees){
+            if(treeheight > mid){
+                sum += (treeheight - mid);
             }
         }
 
         return sum;
     }
+
+
 }
