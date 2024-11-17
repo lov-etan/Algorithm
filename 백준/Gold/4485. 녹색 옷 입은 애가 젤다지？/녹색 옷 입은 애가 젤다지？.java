4bb196204ab7a095fc393cea52a9c28e1674cb1f
Jiwoo <emily1290@naver.com>
2024-11-17 09:39:33 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 244 ms, Memory: 26084 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -2,111 +2,98 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Comparator;
-import java.util.LinkedList;
 import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	//최소 가중치 경로 문제: '다익스트라 알고리즘'
-	
-	static int[] dr = {-1, 1, 0, 0};
-	static int[] dc = {0, 0, -1, 1};
-	
-	static class Pos{
-		int r, c;
-		int sum; 
-		
-		Pos(int r, int c, int sum){
-			this.r = r;
-			this.c = c;
-			this.sum = sum;
-		}
-		
-		public int getSum() {
-			return sum;
-		}
-		
-		@Override
-		public String toString() {
-			return "Pos [r=" + r + ", c=" + c + ", sum=" + sum + "]";
-		}
+    static int N;
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
 
-		
+    static private class Pos{
+        int r, c ;
+        int sum;
 
-		
-		
-	}
-	
-	
-	public static void main(String[] args) throws IOException{
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		int tc=1;
-		
-		while(true) { //0 나올 때까지 테케 돌리기 
-			
-			int N = Integer.parseInt(br.readLine());
-			if(N == 0) {
-				break;
-			}
-			
-			int[][] map = new int[N][N];
-			int[][] dist = new int[N][N]; //최소 비용 저장배열 
-			
-			//dist = 누적합 저장소이자 visited 역할 
-			for(int i=0; i<N; i++) {
-				for(int j=0; j<N; j++) {
-					dist[i][j] = Integer.MAX_VALUE; // 최댓값으로 초기화 
-				}
-			}
-			
-			for(int r=0; r<N; r++) {
-				st = new StringTokenizer(br.readLine());
-				for(int c=0; c<N; c++) {
-					map[r][c] = Integer.parseInt(st.nextToken());
-				}
-			}
-			
-			
-			
-			//가중치 제일 적게 모으고 목적지(N-1, N-1)까지 가는 길은 무엇일까?
-			//다익스트라 알고리즘 시~작! 
-			PriorityQueue<Pos> pq = new PriorityQueue<>(Comparator.comparing(Pos::getSum));	
-			pq.add(new Pos(0,0,map[0][0])); //0,0부터 시작 
-			
-			
-			while(!pq.isEmpty()) {
-				Pos curr = pq.poll();
-				int r  = curr.r;
-				int c = curr.c;
-				
-				for(int d=0; d<4; d++) {
-					int nr = r + dr[d];
-					int nc = c + dc[d];
-					
-					if(nr>=0 && nr<N && nc>=0 && nc<N) {
-						int newSum = curr.sum + map[nr][nc];
-						
-						//다음 갈 곳이 정해지면? 원래 있던 애에서 + 지금 지나갈 cost를 더해줘야겠지?
-						if(newSum < dist[nr][nc]) {
-							dist[nr][nc] = newSum;
-							pq.add(new Pos(nr, nc, newSum));
-						}
-					}
-				}
-				
-			}
-			
-			System.out.println("Problem " + tc++ + ": " + dist[N-1][N-1] );
-			
-		}
-		
-		
-		
-		
-	}
+        public Pos(int r, int c,  int sum){
+            this.r = r;
+            this.c = c;
+            this.sum = sum;
+        }
 
+        public int getSum() { //q에 든 것 중 주변에 어디갈까? -> 루피 모인게 제일 적은 곳을 먼저 고려하자!
+            return sum;
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int tc = 0;
+
+        while(true){
+            tc++;
+            N = Integer.parseInt(br.readLine());
+
+            if(N == 0){
+                break;
+            }
+
+            int[][] map = new int[N][N];
+            for(int r=0; r<N; r++){ //도둑루피 정보 채워넣기
+                st = new StringTokenizer(br.readLine());
+                for(int c=0; c<N; c++){
+                    map[r][c] = Integer.parseInt(st.nextToken());
+                }
+            }
+            ///////////input/////////////////////
+            /// Bfs 시작
+            //다익스트라 - 제일 먼저 도착하는게 아니라 n-1, n-1 위치에서 제일 적은 루피를 모았길 원해
+            int[][] dist = new int[N][N]; //해당 위치일 때 루치 최소 누적액
+            for(int r=0; r<N; r++){
+                for(int c=0; c<N; c++){
+                    dist[r][c] = Integer.MAX_VALUE; //우선 최대값으로 채워넣기
+                }
+            }
+
+            PriorityQueue<Pos> pq = new PriorityQueue<>(Comparator.comparing(Pos::getSum));
+            pq.add(new Pos(0, 0, map[0][0])); // 0,0부터 시작
+            dist[0][0] = map[0][0];
+
+            while(!pq.isEmpty()){
+                Pos curr = pq.poll();
+                int r = curr.r, c = curr.c;
+
+                for(int d=0; d<4; d++){
+                    int nr = r + dr[d];
+                    int nc = c + dc[d];
+
+                    if(inRange(nr, nc)){
+                        int newSum = curr.sum + map[nr][nc];
+
+                        //현재 누적루피가 다음에 갈 곳의 최소누적루피(dist[nr][nc])보다 작으면
+                        //아직 방문한 적 없고, 더 최소로 누적할 수 ㅇ
+                        if(newSum < dist[nr][nc]){
+                            dist[nr][nc] = newSum;
+                            pq.add(new Pos(nr, nc , newSum));
+                        }
+                    }
+                }
+            }
+
+            System.out.println("Problem " + tc + ": " + dist[N-1][N-1]);
+
+        }
+
+
+
+
+    }
+
+    private static boolean inRange(int nr, int nc) {
+        if(nr >= 0 && nr < N && nc >=0 && nc <N){
+            return true;
+        }
+        return false;
+    }
 }
