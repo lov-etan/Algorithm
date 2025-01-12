7a8eaf737e0a464b0889f83985d78646fa31e158
Jiwoo <emily1290@naver.com>
2025-01-12 09:55:00 +0900

    [level 2] Title: 조이스틱, Time: 0.05 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,28 +1,34 @@
 import java.util.*;
 class Solution {
     public int solution(String name) {
-        int alphabet = 0; //위 아래 이동 수
-        int move = name.length()-1; // 좌 우 이동 수 
+        int alphabet = 0;
+        int move = name.length()-1; //좌우 이동 기본값: 처음부터 쭉 가는 것 
         
         for(int i=0; i<name.length(); i++){
-            // 1. 알파벳 선택(순서대로, 역순대로)
+            // 1. 알파벳 조작 순서 cnt
             alphabet += Math.min(name.charAt(i)-'A', 26-(name.charAt(i)-'A'));
             
-            // 2. 위치 선택( < or > ?) - 기본값: 그냥 순서대로 가는 것
-            //중간에 A가 있다면 선택의 기로에 놓이게 됨
-            if(i<name.length()-1 && name.charAt(i+1)=='A') { //만약 중간에 'A'가 등장한다면?
+            // 2. 위치 선택 (좌우) 
+            if(i<name.length()-1 && name.charAt(i+1)=='A') { //중간에 A가 나온다면, 돌아가는게 더 빠를 수도 있다. 
                 
-                //A는 언제 끝나니?
-                int endA = i+1;
-                while(endA < name.length() && name.charAt(endA)=='A') {
+                //A의 끝 위치
+                int endA = i+1; //일단 확인된 시작점
+                while(endA < name.length() && name.charAt(endA)=='A'){
                     endA++;
                 }
-                //왼쪽에서 쭉 가던거 돌아가서 뒤에 endA 이후 것만 취하기 vs 뒤에서부터 갔다가 앞에서 돌기 
-                move = Math.min(move, i*2 + (name.length() - endA));
-                move = Math.min(move, (name.length() - endA)*2 + i);
                 
-            } 
+                //move는 첫 번째 글자에 깜빡깜빡 커서가 되어 있다. 
+                //(1) 정방향으로 갔다가 돌아 나와서 뒤에서부터 endA까지 닿을지, endAAAApoiu
+                //vs (2) 첫 위치에서 <- 로 간 후 역방향으로 돌다가 다시 나와서 앞에서부터 A 시작 전까지 닿을지 endAAAAp
+                move = Math.min(move, i*2 + (name.length() - endA));
+                move = Math.min(move, (name.length()-endA)*2 + i);
+                
+                
+            }
         }
-        return alphabet+move;
+        
+        
+        
+        return alphabet + move;
     }
 }
