9244ae0ad3d8c67ead3b4944b7abef8286cc53c4
Jiwoo <emily1290@naver.com>
2025-06-14 15:07:25 +0900

    [level 1] Title: 과일로 만든 아이스크림 고르기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/133025. 과일로 만든 아이스크림 고르기/과일로 만든 아이스크림 고르기.sql
+++ 프로그래머스/1/133025. 과일로 만든 아이스크림 고르기/과일로 만든 아이스크림 고르기.sql
@@ -1,6 +1,5 @@
--- 상반기 총주문량 > 3000 && 아이스크림 주성분 = '과일' && Order by 총주문량 DESC
-SELECT fh.flavor
-FROM FIRST_HALF as fh
-JOIN ICECREAM_INFO as ii ON fh.flavor = ii.flavor
-WHERE fh.total_order > 3000 AND ii.ingredient_type = 'fruit_based'
-ORDER BY fh.TOTAL_ORDER DESC;
+SELECT a.flavor
+FROM first_half as a
+JOIN icecream_info as b ON a.flavor = b.flavor
+WHERE a.total_order > 3000 AND b.ingredient_type = 'fruit_based'
+ORDER BY a.total_order DESC
