1aa0d18717a679f3e058284158c8a5917e7ea9f1
Jiwoo <emily1290@naver.com>
2025-06-18 14:58:36 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 8 ms, Memory: 2268 KB -BaekjoonHub

---
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+deque<char> dq;
+
+void popDq() {
+    while(!dq.empty()) {
+        cout << dq.front();
+        dq.pop_front();
+    }
+}
+
+int main() {
+    string s;
+    getline(cin, s);
+
+    int idx = 0;
+    while(idx < s.length()) {
+        if(s[idx] == '<') {
+            popDq();
+            while(s[idx] != '>') {
+                dq.push_back(s[idx]);
+                idx++;
+            }
+            dq.push_back(s[idx]);
+            popDq();
+        }
+        // else if(s[idx] == '>') { // 뒤에서 ++ 이 하나 더 적용돼서
+        //     dq.push_back(s[idx]);
+        //     popDq();
+        // }
+        else if(s[idx] == ' ') {
+            popDq();
+            cout << ' ';
+        }
+        else {
+            dq.push_front(s[idx]);
+        }
+
+        idx++;
+    }
+
+    popDq();
+    
+    return 0;
+}
