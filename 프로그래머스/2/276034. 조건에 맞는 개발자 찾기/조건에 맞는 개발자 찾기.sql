046894d43d169259a3cf917fea1e7d742e160cb2
Jiwoo <emily1290@naver.com>
2025-06-23 20:36:00 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,8 @@
+SELECT id, email, first_name, last_name
+FROM developers
+WHERE
+    skill_code & (SELECT code FROM skillcodes WHERE name = 'C#')
+    OR
+    skill_code & (SELECT code FROM skillcodes WHERE name = 'Python')
+ORDER BY 1
+
