467044f53a72e9cde0d43fd8c568256a2a80bf95
Jiwoo <emily1290@naver.com>
2025-06-29 21:52:56 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,6 +1,7 @@
--- 코드를 입력하세요
-SELECT ins.animal_id, outs.name
-FROM animal_ins as ins 
-    JOIN animal_outs as outs ON ins.animal_id = outs.animal_id
-WHERE ins.datetime  > outs.datetime
-ORDER BY ins.datetime ASC;
+# 보호 시작일(ins) > 입양일 (outs)
+SELECT ins.animal_id, ins.name
+FROM animal_ins as ins
+    JOIN animal_outs as outs
+    ON ins.animal_id = outs.animal_id
+WHERE ins.datetime > outs.datetime
+ORDER BY ins.datetime 
