323b2a9bb59b151501edab4c1c5129c75fd7f615
Jiwoo <emily1290@naver.com>
2025-06-29 22:13:02 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,6 +1,7 @@
--- 보호소 들어오기 전 중성화 x | 보호소 들어온 후 중성화 o 
+# INS(중성화x - Intact) OUTS(중성화O - Spayed, Neutered) 인 친구들
 SELECT ins.animal_id, ins.animal_type, ins.name
-FROM animal_outs as outs
-    JOIN animal_ins as ins ON outs.animal_id = ins.animal_id
-WHERE LEFT(outs.sex_upon_outcome, 6) IN ('Spayed', 'Neuter') 
-    AND LEFT(ins.sex_upon_intake, 6) = 'Intact'
+FROM animal_ins as ins
+    JOIN animal_outs as outs
+    ON ins.animal_id = outs.animal_id
+WHERE ins.sex_upon_intake LIKE 'Intact%' 
+    AND (outs.sex_upon_outcome LIKE 'Spayed%' OR outs.sex_upon_outcome LIKE 'Neutered%')
