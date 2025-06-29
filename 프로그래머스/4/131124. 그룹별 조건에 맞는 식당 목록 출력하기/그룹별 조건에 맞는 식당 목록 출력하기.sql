64aa9657d72e16a294f9d0bd41100028c6708033
Jiwoo <emily1290@naver.com>
2025-06-29 21:08:45 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,14 +1,10 @@
--- 문제: 가장 많이 작성한 회원의 '리뷰들'이 나와야 하는데..
-SELECT p.member_name, r.review_text, date_format(r.review_date, "%Y-%m-%d") as review_date
-FROM member_profile as p
-    JOIN rest_review as r ON p.member_id = r.member_id
-WHERE p.member_id = (SELECT member_id
-        FROM rest_review
-        GROUP BY member_id
-        ORDER By count(member_id) desc
-        LIMIT 1)
-ORDER BY review_date, review_text;
-
--- 일단 가장 많이 작성한 회원(1명)을 구해보자 
-
--- group by는 '집계함수'와 쓰임!!
+# 리뷰를 가장 많이 작성한 회원 1명 \ 그의 리뷰들 
+SELECT p.member_name, r.review_text, DATE_FORMAT(r.review_date, "%Y-%m-%d") as review_date
+FROM member_profile as p 
+JOIN rest_review as r 
+    ON p.member_id = r.member_id
+JOIN (SELECT member_id FROM rest_review 
+      GROUP BY member_id 
+      ORDER BY COUNT(*) DESC LIMIT 1) as max_r
+     ON r.member_id = max_r.member_id
+ORDER BY r.review_date, r.review_text
