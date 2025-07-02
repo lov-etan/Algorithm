99b42f559331bb38f9d4bfa5c5200843b693f8d2
Jiwoo <emily1290@naver.com>
2025-07-03 01:33:58 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -1,14 +1,10 @@
--- 취소되지 않은 진정한 진료예약내역 <- 2022년 4월 13일 
- SELECT a.apnt_no, p.pt_name, p.pt_no, d.mcdp_cd, d.dr_name, a.apnt_ymd
- FROM patient as p 
-     JOIN appointment as a ON p.pt_no = a.pt_no
-     JOIN doctor as d ON d.dr_id = a.mddr_id
- WHERE a.apnt_no in (
-     select apnt_no 
-    from appointment
-    where apnt_cncl_yn = 'N' AND mcdp_cd = 'CS'
-    AND date(apnt_ymd) = '2022-04-13'
-    ) 
-ORDER BY apnt_ymd asc;
+-- 2022-04-13 취소되지 않은 'CS' 진료 예약 내약
 
-    
+SELECT a.apnt_no, p.pt_name, p.pt_no, d.mcdp_cd, d.dr_name, a.apnt_ymd
+FROM appointment as a
+    LEFT JOIN patient as p ON a.pt_no = p.pt_no
+    LEFT JOIN doctor as d ON a.mddr_id = d.dr_id
+WHERE DATE_FORMAT(a.apnt_ymd, "%Y-%m-%d") = '2022-04-13' 
+    AND a.apnt_cncl_yn = 'N'
+    AND d.mcdp_cd = 'CS'
+ORDER BY 6 
