4e4346780b4da5961abc416bd689a50f8b9b4f00
Jiwoo <emily1290@naver.com>
2025-07-21 16:43:40 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 40 ms, Memory: 6644 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -8,32 +8,32 @@ int N, K;
 
 int main() {
     cin >> N >> K;
-    
-    priority_queue< pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> > pq;
+    queue<pair<int,int>> q;
     set<int> viss;
-    pq.push({0, N});
+    q.push({N, 0});
 
-    while(!pq.empty()) {
-        pair<int,int> curr = pq.top(); pq.pop();
-        int t = curr.first;
-        int x = curr.second;
+    while(!q.empty()) {
+        pair<int,int> curr = q.front(); q.pop();
+        int x = curr.first;
+        int t = curr.second;
+
 
         if(x==K) {
             cout << t;
             return 0;
         }
-
+        
         if(2*x <= 100000 && !viss.count(2*x)) {
             viss.insert(2*x);
-            pq.push({t, 2*x});
+            q.push({2*x, t});
         }
         if(x-1>=0 && !viss.count(x-1)) {
             viss.insert(x-1);
-            pq.push({t+1, x-1});
+            q.push({x-1, t+1});
         }
         if(x+1 <= 100000 && !viss.count(x+1)) {
             viss.insert(x+1);
-            pq.push({t+1, x+1});
+            q.push({x+1, t+1});
         }
         
     }
