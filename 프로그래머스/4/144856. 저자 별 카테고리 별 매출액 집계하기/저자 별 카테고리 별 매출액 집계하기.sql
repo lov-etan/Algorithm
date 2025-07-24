cde4dddf052d035659969398ec513fe759d031de
Jiwoo <emily1290@naver.com>
2025-07-24 15:55:18 +0900

    [level 4] Title: 저자 별 카테고리 별 매출액 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
+++ 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
@@ -1,9 +1,22 @@
-SELECT a.author_id, a.author_name, b.category, SUM(bs.sales*b.price) as total_sales
-FROM book as b
-    JOIN author as a
-    ON b.author_id = a.author_id
-    JOIN book_sales as bs
-    ON b.book_id = bs.book_id
-WHERE bs.SALES_DATE LIKE '2022-01%'
-GROUP BY 1, 3
-ORDER BY 1 , 3 DESC
+WITH filteredBS AS (
+    SELECT book_id, SUM(SALES) as sum_sales
+    FROM BOOK_SALES
+    WHERE YEAR(sales_date) = '2022' AND MONTH(sales_date) = '01'
+    GROUP BY book_id
+)
+
+# 저자ID별, 카테고리별 '매출액(판매량 * 판매가)'
+SELECT a.author_id, a.author_name, b.category, 
+        SUM(b.price * bs.sum_sales) as total_sales
+FROM BOOK as b
+    JOIN AUTHOR as a ON b.author_id = a.author_id
+    JOIN filteredBS as bs ON b.book_id = bs.book_id
+GROUP BY 1,2,3 
+ORDER BY 1,3 DESC;
+
+# SELECT *, SUM(SALES) as sum_sales
+# FROM BOOK_SALES
+# WHERE YEAR(sales_date) = '2022' AND MONTH(sales_date) = '01'
+# GROUP BY book_id
+    
+# SELECT * FROM BOOK_SALES WHERE YEAR(sales_date) = '2022' AND MONTH(sales_date) = '01' ORDER BY book_id;
