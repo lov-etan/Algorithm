ecd1ad1bb548e7c56799ab5f70d8c8e23cb33ae7
Jiwoo <emily1290@naver.com>
2025-08-03 01:25:27 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -0,0 +1,19 @@
+WITH 
+cte AS (
+    SELECT emp_no, year, SUM(score) as sum_score
+    FROM hr_grade
+    WHERE year = '2022'
+    GROUP BY 1
+),
+cte1 AS (
+    SELECT MAX(sum_score) as max_score
+    FROM cte
+)
+
+# # 2022년도 한해 평가 점수가 가장 높은 사원 정보
+SELECT distinct c.sum_score as score, c.emp_no, e.emp_name, e.position, e.email
+FROM HR_EMPLOYEES as e
+    JOIN HR_GRADE as g ON e.emp_no = g.emp_no
+    JOIN cte as c ON g.emp_no = c.emp_no
+WHERE g.year = '2022'
+    AND c.sum_score = (SELECT max_score FROM cte1)
