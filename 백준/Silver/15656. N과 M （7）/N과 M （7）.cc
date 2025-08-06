83f0973b098b83ab842f714aaaff13a9a7921f03
Jiwoo <emily1290@naver.com>
2025-08-06 09:14:39 +0900

    [Silver III] Title: N과 M (7), Time: 424 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+vector<int> nums;
+vector<int> picks;
+
+void dfs(int sIdx) {
+    if(sIdx == M) {
+        for(auto num : picks) {
+            cout << num << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for(int i=0; i<N; i++) {
+        picks.push_back(nums[i]);
+        dfs(sIdx+1);
+        picks.pop_back();
+    }
+
+    return;
+}
+
+int main() {
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
+    cin >> N >> M;
+    nums.resize(N,0);
+
+    for(int i=0; i<N; i++) {
+        cin >> nums[i];
+    }
+
+    sort(nums.begin(), nums.end());
+    
+    dfs(0);
+    
+    return 0;
+}
