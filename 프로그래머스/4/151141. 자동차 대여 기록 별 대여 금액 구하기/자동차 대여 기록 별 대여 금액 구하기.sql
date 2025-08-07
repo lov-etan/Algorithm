ceb475a237826e6fae04b6b0a1cd5e4b6a3a49c3
Jiwoo <emily1290@naver.com>
2025-08-07 19:48:53 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,23 +1,31 @@
--- HISTORY_ID -> car_id = 1 (트럭)
--- end_date,start_date+1 case when >= 7 THEN '7일이상' && car_type = 위
-
-WITH RENT AS (
-    SELECT car.car_id, car.car_type, car.daily_fee, his.history_id, his.start_date, his.end_date,
-        CASE
-        WHEN DATEDIFF(END_DATE, START_DATE)+1 >= 90 THEN '90일 이상'
-        WHEN DATEDIFF(END_DATE, START_DATE)+1 >= 30 THEN '30일 이상'
-        WHEN DATEDIFF(END_DATE, START_DATE)+1 >= 7 THEN '7일 이상'
-        ELSE '7일 미만'
-        END as duration_type
-    FROM car_rental_company_car as car
-    JOIN car_rental_company_rental_history as his
-    ON car.car_id = his.car_id
-    WHERE car.car_type = '트럭'
+# 차 종류 = '트럭'
+# 대여 기록(history_id) 별 대여 금액(Fee)
+WITH 
+history AS (
+    SELECT
+        (
+            CASE
+            WHEN DATEDIFF(end_date, start_date)+1 >= 90 THEN '90일 이상'
+            WHEN DATEDIFF(end_date, start_date)+1 >= 30 THEN '30일 이상'
+            WHEN DATEDIFF(end_date, start_date)+1 >= 7 THEN '7일 이상'
+            ELSE '7일 미만'
+            END
+        ) as duration_type, DATEDIFF(end_date, start_date)+1 as duration, history_id, h.car_id, c.daily_fee, c.car_type
+    FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY as h
+    JOIN CAR_RENTAL_COMPANY_CAR as c
+    ON h.car_id = c.car_id
+    WHERE car_type = '트럭'
 )
 
-SELECT distinct rent.history_id, 
-    FLOOR(rent.daily_fee * (1-IFNULL(plan.discount_rate,0)/100))*(DATEDIFF(rent.end_date, rent.start_date)+1) as fee
-FROM car_rental_company_discount_plan as plan
-RIGHT JOIN rent 
-ON plan.car_type = rent.car_type AND plan.duration_type = rent.duration_type
+# h.history_id, ROUND((c.daily_fee * (1 - p.discount_rate/100) * h.duration),0) as fee
+SELECT 
+        h.history_id, 
+        FLOOR((h.daily_fee * (1 - IFNULL(p.discount_rate/100, 0)) * h.duration)) as fee
+FROM history as h
+LEFT JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN as p
+ON h.car_type = p.car_type AND h.duration_type = p.duration_type
+GROUP BY 1
 ORDER BY 2 DESC, 1 DESC
+
+# SELECT * FROM history
+# SELECT * FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN
