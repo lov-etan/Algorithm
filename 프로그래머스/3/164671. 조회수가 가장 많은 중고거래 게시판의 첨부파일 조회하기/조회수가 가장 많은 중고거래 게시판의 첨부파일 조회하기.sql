6fda617b6173531bc0f52480abbf745f21568022
Jiwoo <emily1290@naver.com>
2025-08-07 18:23:37 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,6 +1,17 @@
-SELECT CONCAT('/home/grep/src/', f.board_id, '/', f.file_id, f.file_name ,f.file_ext) as file_path
-FROM used_goods_board as b
-    JOIN used_goods_file as f
-    ON b.board_id = f.board_id
-WHERE b.board_id = (SELECT board_id FROM used_goods_board ORDER BY views DESC LIMIT 1)
-ORDER BY f.file_id DESC
+# 가장 조회수가 높은 게시물?
+# 그 게시물의 첨부파일 경로!
+ WITH 
+ cte AS (
+    SELECT MAX(views) as max_view
+    FROM USED_GOODS_BOARD
+ ), 
+ cte2 AS (
+    SELECT board_id
+    FROM USED_GOODS_BOARD
+    WHERE views = (SELECT max_view FROM cte)
+ )
+
+SELECT concat('/home/grep/src/', board_id, '/', file_id, file_name, file_ext) as file_path
+FROM USED_GOODS_FILE as f
+WHERE board_id = (SELECT board_id FROM cte2)
+ORDER BY file_id DESC
