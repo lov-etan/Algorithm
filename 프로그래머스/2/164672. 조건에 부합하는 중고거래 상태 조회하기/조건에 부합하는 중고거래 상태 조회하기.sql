dc54d1799bc61d31a684f1fe614ba13c371ea1ed
Jiwoo <emily1290@naver.com>
2025-08-11 18:45:28 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -1,8 +1,12 @@
-SELECT board_id, writer_id, title, price, 
-    (CASE 
-     WHEN status = 'SALE' THEN '판매중'
-     WHEN status = 'RESERVED' THEN '예약중'
-     ELSE '거래완료' END) as status
-FROM used_goods_board
-WHERE DATE_FORMAT(created_date, "%Y-%m-%d") = '2022-10-05'
+# 2022년 10월 5일에 등록된 중고거래 조회 / 거래상태 매핑 
+SELECT  board_id, writer_id, title, price, 
+        (
+            CASE
+            WHEN status = 'SALE' THEN '판매중'
+            WHEN status = 'RESERVED' THEN '예약중'
+            WHEN status = 'DONE' THEN '거래완료'
+            END
+        ) as status
+FROM USED_GOODS_BOARD
+WHERE created_date = '2022-10-05'
 ORDER BY 1 DESC
