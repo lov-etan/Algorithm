161cfabb064b1ed27148791a147989f9ec28f10d
Jiwoo <emily1290@naver.com>
2025-08-13 13:54:46 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -1,4 +1,5 @@
-SELECT animal_id, name, sex_upon_intake
-FROM animal_ins
-WHERE name IN ('Lucy', "Ella", "Pickle", 'Rogan', 'Sabrina', 'Mitty')
+# 이름이 Lucy, Ella, Pickle, Rogan, Sabrina, Mitty인 동물
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+FROM ANIMAL_INS
+WHERE NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
 ORDER BY 1
