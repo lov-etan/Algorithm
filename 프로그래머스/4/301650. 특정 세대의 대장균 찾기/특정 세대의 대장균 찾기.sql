21449738b44089a1190dedcc50337ab75ba46437
Jiwoo <emily1290@naver.com>
2025-08-19 05:52:05 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,9 +1,29 @@
 # 3세대를 찾아라
+# 풀이 1
+# SELECT ID
+# FROM ECOLI_DATA
+# WHERE PARENT_ID IN (SELECT ID
+#                     FROM ECOLI_DATA
+#                     WHERE PARENT_ID IN (SELECT ID
+#                                         FROM ECOLI_DATA
+#                                         WHERE PARENT_ID IS NULL))
+# ORDER BY 1
+
+# 풀이2
+WITH RECURSIVE cte AS (
+    SELECT  ID, PARENT_ID, 
+            1 as DEPTH
+    FROM    ECOLI_DATA
+    WHERE   parent_id IS NULL
+    
+    UNION ALL
+    
+    SELECT  e.ID, e.PARENT_ID, c.depth +1
+    FROM    ECOLI_DATA as e JOIN cte as c ON e.PARENT_ID = c.ID
+    WHERE   depth < 3
+)
+
 SELECT ID
-FROM ECOLI_DATA
-WHERE PARENT_ID IN (SELECT ID
-                    FROM ECOLI_DATA
-                    WHERE PARENT_ID IN (SELECT ID
-                                        FROM ECOLI_DATA
-                                        WHERE PARENT_ID IS NULL))
+FROM CTE
+WHERE DEPTH = 3
 ORDER BY 1
