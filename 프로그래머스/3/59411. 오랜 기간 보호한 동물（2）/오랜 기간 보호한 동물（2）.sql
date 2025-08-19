5d1833d3b1b13cb8459f2acf64a8b5f6ad05668d
Jiwoo <emily1290@naver.com>
2025-08-19 10:08:46 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,12 +1,18 @@
-# 보호 기간이 가장 길었던 두 마리
-WITH cte AS (
-    SELECT i.animal_id, i.name, DATEDIFF(o.datetime, i.datetime)+1 as diff
-    FROM ANIMAL_INS as i
-        JOIN ANIMAL_OUTS as o ON i.animal_id = o.animal_id
-)
+# # 보호 기간이 가장 길었던 두 마리
+# WITH cte AS (
+#     SELECT i.animal_id, i.name, DATEDIFF(o.datetime, i.datetime)+1 as diff
+#     FROM ANIMAL_INS as i
+#         JOIN ANIMAL_OUTS as o ON i.animal_id = o.animal_id
+# )
 
-SELECT animal_id, name
-FROM cte
-ORDER BY diff DESC
+# SELECT animal_id, name
+# FROM cte
+# ORDER BY diff DESC
+# LIMIT 2
+
+SELECT I.ANIMAL_ID, I.NAME 
+FROM ANIMAL_INS I 
+JOIN ANIMAL_OUTS O 
+ON I.ANIMAL_ID = O.ANIMAL_ID 
+ORDER BY DATEDIFF(I.DATETIME, O.DATETIME) 
 LIMIT 2
-
