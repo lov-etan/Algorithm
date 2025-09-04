c969b626cc5142d413577e83e1bea56c26dde3a2
Jiwoo <emily1290@naver.com>
2025-09-04 21:17:02 +0900

    [level 4] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,11 +1,26 @@
--- 코드를 입력하세요
-SELECT distinct car.car_id, car.car_type, ROUND(30 * car.daily_fee * (100 - plan.discount_rate) / 100) as fee
-FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY as his 
-    JOIN(CAR_RENTAL_COMPANY_CAR as car 
-            JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN as plan ON car.car_type = plan.car_type)
-    ON his.car_id = car.car_id
-WHERE car.car_type IN ('세단', 'SUV') AND plan.duration_type = '30일 이상'
-    AND car.car_id NOT IN(SELECT car_id FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY WHERE 
-                            start_date <= '2022-11-30' AND end_date >= '2022-11-01')
-HAVING fee >= 500000 AND fee < 2000000
-ORDER BY fee desc, car.car_type, car.car_id DESC;
+# 종류 = 세단, SUV | 2022 11월
+# 할인 적용 * 30 * 금액 -> 총 50-200만원
+WITH
+nov AS (
+    SELECT car_id
+    FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+    WHERE start_date <='2022-11-30' AND end_date >= '2022-11-01'
+)
+, cte AS (
+    SELECT c.car_id, c.car_type, c.daily_fee
+    FROM   CAR_RENTAL_COMPANY_CAR as c
+            JOIN CAR_RENTAL_COMPANY_RENTAL_HISTORY as h
+            ON c.car_id = h.car_id
+    WHERE  c.car_id NOT IN (SELECT car_id FROM nov)
+            AND c.car_type IN ('SUV', '세단')
+)
+
+
+SELECT distinct c.car_id, c.car_type, FLOOR((c.daily_fee*30*(1-p.discount_rate/100))) as fee
+FROM cte as c
+        JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN as p
+        ON c.car_type = p.car_type
+WHERE p.duration_type = '30일 이상'
+        AND c.daily_fee*30*(1-p.discount_rate/100) >= 500000
+        AND c.daily_fee*30*(1-p.discount_rate/100) < 2000000
+ORDER BY 3 DESC, 2, 1 DESC
