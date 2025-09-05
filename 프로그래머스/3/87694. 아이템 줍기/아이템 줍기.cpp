b29bbc3c2c5775e80aee38a794bd3718f49cd07e
Jiwoo <emily1290@naver.com>
2025-09-05 13:06:29 +0900

    [level 3] Title: 아이템 줍기, Time: 0.02 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.cpp
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.cpp
@@ -1,80 +1,68 @@
 #include <string>
 #include <vector>
-#include <iostream>
 #include <queue>
 
 using namespace std;
+int cX, cY, iX, iY;
+vector<vector<int>> maps(101, vector<int>(101,0));
+vector<vector<int>> dp(101, vector<int>(101,-1));
+queue<pair<int,int>> q;
 
-int dr[] = {1,0,-1,0};
-int dc[] = {0,1,0,-1};
+int dr[] = {-1,0,1,0};
+int dc[] = {0, 1, 0, -1};
 
 bool inRange(int r, int c) {
     return r>=0 && r<101 && c>=0 && c<101;
 }
 
-int solution(vector<vector<int>> rectangle, int characterX, int characterY, int itemX, int itemY) {
-    int answer = 0;
-    vector<vector<int>> maps(101,vector<int>(101,0));
-    
-    for(int i=0; i<rectangle.size(); i++) {
-        int sr = rectangle[i][0]*2; int sc = rectangle[i][1]*2; 
-        int er = rectangle[i][2]*2; int ec = rectangle[i][3]*2;
-        
-        for(int r=sr; r<=er; r++) {
-            for(int c=sc; c<=ec; c++) {
-                maps[r][c] = 1;
-            }
-        }
-    }
-    
-    for(int i=0; i<rectangle.size(); i++) {
-        int sr = rectangle[i][0]*2; int sc = rectangle[i][1]*2; 
-        int er = rectangle[i][2]*2; int ec = rectangle[i][3]*2;
-        
-        for(int r=sr+1; r<er; r++) {
-            for(int c=sc+1; c<ec; c++) {
-                maps[r][c] = 0; // 안에 비우기 
-            }
-        }
-    }
-    
-    // for(int r=0; r<51; r++) {
-    //     for(int c=0; c<51; c++) {
-    //         cout  << maps[r][c];
-    //     }
-    //     cout << "\n";
-    // }
-    
-    queue<pair<int,int>> q;
-    vector<vector<int>> dist(101,vector<int>(101,0));
-    q.push({characterX*2,characterY*2});
-    dist[characterX*2][characterY*2] = 1;
+int bfs() {
+    dp[cX][cY] = 0;
+    q.push({cX, cY});
     
     while(!q.empty()) {
-        pair<int,int> pos = q.front(); q.pop();
-        int r = pos.first;
-        int c = pos.second;
-        
-        if(r == itemX*2 && c == itemY*2) break;
+        auto [r,c] = q.front(); q.pop();
+        if(r==iX && c==iY) {
+            return dp[r][c];
+        }
         
         for(int d=0; d<4; d++) {
-            int nr = r+dr[d];
-            int nc = c+dc[d];
-            
-            if(inRange(nr, nc) && dist[nr][nc] == 0 && maps[nr][nc] == 1) {
-                cout << nr << " " << nc << "\n";
-                dist[nr][nc] = dist[r][c]+1;
+            int nr = r + dr[d];
+            int nc = c + dc[d];
+            if(inRange(nr,nc) && maps[nr][nc] == 1 && dp[nr][nc] == -1) {
+                dp[nr][nc] = dp[r][c] + 1;
                 q.push({nr,nc});
             }
         }
     }
+}
+
+int solution(vector<vector<int>> rectangle, int characterX, int characterY, int itemX, int itemY) {
+    int answer = 0;
+    cX = 2 * characterX; cY = 2 * characterY; iX = 2 * itemX; iY = 2 * itemY;
     
-//     for(int r=0; r<51; r++) {
-//         for(int c=0; c<51; c++) {
-//             cout  << dist[r][c];
-//         }
-//         cout << "\n";
-//     }
+    // 모두 1로 채워주기
+    for(int i=0; i<rectangle.size(); i++) {
+        int r1 = rectangle[i][0] * 2; int c1 = rectangle[i][1] * 2; 
+        int r2 = rectangle[i][2] * 2; int c2 = rectangle[i][3] * 2;
+        
+        for(int r=r1; r<=r2; r++) {
+            for(int c=c1; c<=c2; c++) {
+                maps[r][c] = 1; 
+            }
+        }
+    }
     
-     return dist[itemX*2][itemY*2]/2; //좌표 2배 확장했기에 줄이기
+    // 테두리 안은 0으로 비워주기
+    for(int i=0; i<rectangle.size(); i++) {
+        int r1 = rectangle[i][0] * 2; int c1 = rectangle[i][1] * 2; 
+        int r2 = rectangle[i][2] * 2; int c2 = rectangle[i][3] * 2;
+        
+        for(int r=r1+1; r<=r2-1; r++) {
+            for(int c=c1+1; c<=c2-1; c++) {
+                maps[r][c] = 0; 
+            }
+        }
+    }
+    
+    return bfs()/2;
 }
