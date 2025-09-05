defb87aa8a8769f511069fb2938745714185086f
Jiwoo <emily1290@naver.com>
2025-09-05 10:08:52 +0900

    [level 3] Title: 단어 변환, Time: 0.02 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -1,56 +1,47 @@
 #include <string>
 #include <vector>
-#include <iostream>
 #include <queue>
 
 using namespace std;
+queue<pair<string,int>> q;
+vector<bool> vis;
+vector<string> wrds;
+string t;
 
-bool changeOne(string a, string b) {
+bool Diff1(string a, string b) {
     int cnt = 0;
     for(int i=0; i<a.length(); i++) {
         if(a[i] != b[i]) cnt++;
     }
     
-    if(cnt == 1) {
-        return true;
-    } else {
-        return false;
-    }
+    if(cnt==1) return true;
+    return false;
 }
 
-int solution(string begin, string target, vector<string> words) {
-    int answer = 0;
-    int N = words.size();
+int bfs(string begin) {
+    q.push({begin, 0});
     
-    // 반환 불가
-    int impos = 0;
-    for(int i=0; i<N; i++) {
-        if(words[i] == target) impos++;
-    }
-    if(impos == 0) return 0;
-    
-    vector<bool> visited(N, false);
-    queue<pair<string, int>> q;
-    q.push({begin,0});
     while(!q.empty()) {
-        pair<string, int> curr = q.front(); q.pop();
-        string word = curr.first;
-        int cnt = curr.second;
-        
-        if(word == target) {
+        auto [curr, cnt] = q.front(); q.pop();
+        if(curr == t) {
             return cnt;
         }
         
-        for(int i=0; i<N; i++) {
-            if(!visited[i] && changeOne(word, words[i])) {
-                visited[i] = true;
-                q.push({words[i], cnt+1});
-            }
+        for(int i=0; i<wrds.size(); i++) {
+            if(!vis[i] && Diff1(wrds[i], curr)) {
+                vis[i] = true;
+                q.push({wrds[i], cnt+1});
+             }
         }
-        
     }
     
+    return 0;
+}
+
+int solution(string begin, string target, vector<string> words) {
+    vis.resize(words.size(), false);
+    wrds = words;
+    t = target;
     
-    
-    return answer;
+    return bfs(begin);
 }
