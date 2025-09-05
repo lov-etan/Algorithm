9f365bf0f5473139719fcd44b60a80efc127652b
Jiwoo <emily1290@naver.com>
2025-09-05 09:38:33 +0900

    [level 3] Title: 네트워크, Time: 0.07 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -3,27 +3,38 @@
 #include <queue>
 
 using namespace std;
+int N;
+vector<vector<int>> coms;
+vector<bool> vis;
+queue<int> q;
+
+void bfs(int start) {
+    vis[start] = true;
+    q.push(start);
+    
+    while(!q.empty()) {
+        int curr = q.front(); q.pop();
+        
+        for(int nxt=0; nxt<N; nxt++) {
+            if(!vis[nxt] && coms[curr][nxt]==1) {
+                vis[nxt] = true;
+                q.push(nxt);
+            }
+        }
+    }
+}
 
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
-    vector<bool> visited(n,false);
+    coms = computers; N = n;
+    vis.resize(n,false);
     
     for(int i=0; i<n; i++) {
-        if(!visited[i]) {            
-            queue<int> q;
-            q.push(i);
-            while(!q.empty()) {
-                int curr = q.front(); q.pop();
-                visited[curr] = true;
-                
-                for(int j=0; j<n; j++) {
-                    if(!visited[j] && computers[curr][j] == 1) {
-                        q.push(j);
-                    }
-                }
-            }
+        if(!vis[i]) {
+            bfs(i);
             answer++;
         }
     }
+    
     return answer;
 }
