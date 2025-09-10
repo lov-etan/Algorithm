fce9ebb86550e6dcc9d4aab2a97d80f339a9d01d
Jiwoo <emily1290@naver.com>
2025-09-11 00:45:25 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,9 +1,9 @@
-# 아직 입양 못 간 동물 - outs엔 없는 애들 is null
-# 가장 오래된 친구 3마리
-SELECT ins.name, ins.datetime
-FROM animal_ins as ins
-    LEFT JOIN animal_outs as outs
-    ON ins.animal_id = outs.animal_id
-WHERE outs.animal_id IS NULL
-ORDER BY ins.datetime
+-- 아직 입양 못 간 동물 중
+-- 가장 오래 보호소에 있었던 동물 3마리~!
+SELECT i.name, i.datetime
+FROM ANIMAL_INS as i
+    LEFT JOIN ANIMAL_OUTS as o
+    ON i.animal_id = o.animal_id
+WHERE o.animal_id IS NULL
+ORDER BY i.datetime
 LIMIT 3
