471b56f6caae6b112b4d1b6ff0e8acf8fafbeb28
Jiwoo <emily1290@naver.com>
2025-09-12 02:45:54 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -1,18 +1,17 @@
-# 2021년에 가입 + 상품을 구매한 회원수
-# 비율: 2021년에 가입한 회원 중 상품을 구매한 회원수 / 2021년에 가입한 전체 회원 수
-# year, month, perchased_users, purchased_ratio
-WITH cnt_2021 AS (
-    SELECT distinct *, count(*) as cnt
-    FROM user_info
-    WHERE YEAR(joined) = '2021'
+# 2021년에 가입한 전체 회원들 중 
+# 2021년에 가입한 회원 중 상품을 구매한 회원수 / 2021년에 가입한 전체 회원 수
+WITH cte AS (
+    SELECT COUNT(user_id) as cnt
+    FROM USER_INFO
+    WHERE JOINED LIKE '2021%'
 )
 
-SELECT YEAR(s.sales_date) as year, MONTH(s.sales_date) as month,
-    count(distinct u.user_id) as purchased_users,
-    ROUND((count(distinct u.user_id)/(SELECT cnt FROM cnt_2021)),1) as purchased_ratio
-FROM user_info as u
-    JOIN online_sale as s
-    ON u.user_id = s.user_id
-WHERE YEAR(u.joined) = '2021'
-GROUP BY YEAR(s.sales_date), MONTH(s.sales_date)
-ORDER BY 1, 2
+SELECT  YEAR(o.SALES_DATE) as year, 
+        MONTH(o.SALES_DATE) as month, 
+        COUNT(distinct u.user_id) as PURCHASED_USERS,
+        ROUND(COUNT(distinct u.user_id) / (SELECT cnt FROM cte),1) as PUCHASED_RATIO
+FROM ONLINE_SALE as o
+    JOIN USER_INFO as u ON o.user_id = u.user_id
+WHERE u.JOINED  LIKE '2021%'
+GROUP BY 1,2
+ORDER BY 1,2
