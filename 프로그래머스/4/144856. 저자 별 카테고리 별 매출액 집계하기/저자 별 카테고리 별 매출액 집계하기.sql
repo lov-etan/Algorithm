8e2f462cf463900b6699b6b3deee30cde353ad93
Jiwoo <emily1290@naver.com>
2025-09-18 19:53:30 +0900

    [level 4] Title: 저자 별 카테고리 별 매출액 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
+++ 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
@@ -1,17 +1,8 @@
-# 2022년 1월 기준
-# 저자 별, 카테고리 별 매출액
-WITH cte AS (
-    SELECT book_id, sum(sales) as sum_sales
-    FROM BOOK_SALES
-    WHERE SALES_DATE LIKE '2022-01%'
-    GROUP BY 1
-)
-
-SELECT a.author_id, a.author_name, b.category, SUM(b.PRICE*c.sum_sales) as total_sales
+# 2022년 1월
+SELECT b.AUTHOR_ID, a.AUTHOR_NAME, b.CATEGORY, SUM(b.price*s.sales) as TOTAL_SALES
 FROM BOOK as b
-    JOIN AUTHOR as a ON b.author_id = a.author_id
-    JOIN cte as c ON b.book_id = c.book_id
-GROUP BY 1,3
+    JOIN AUTHOR as a ON b.AUTHOR_ID	 = a.AUTHOR_ID
+    JOIN BOOK_SALES as s ON b.BOOK_ID = s.BOOK_ID
+WHERE SALES_DATE LIKE '2022-01%'
+GROUP BY 1, 3
 ORDER BY 1, 3 DESC
-
-
