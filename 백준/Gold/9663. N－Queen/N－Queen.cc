3480039df88cab5e29e19b7637b8b1e173b4c9ea
Jiwoo <emily1290@naver.com>
2025-09-26 18:17:38 +0900

    [Gold IV] Title: N-Queen, Time: 3816 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -2,66 +2,50 @@
 #include <vector>
 
 using namespace std;
+int N; int ans;
+vector<bool> Rvis;
+vector<bool> Cvis;
+vector<bool> cross1;
+vector<bool> cross2;
 
-int N; int ans = 0;
-vector<vector<bool>> maps;
-vector<bool> visCol;
-vector<bool> visRow;
-
-int dr[] = {-1,-1};
-int dc[] = {-1,1};
-
-bool inRange(int r, int c) {
-    return r>=0 && r<N && c>=0 && c<N;
+void vis(int r, int c, int flag) {
+    Rvis[r] = flag;
+    Cvis[c] = flag;
+    cross1[N+r-c] = flag;
+    cross2[r+c] = flag;
 }
 
-bool checkDiag(int r, int c) {
-    for(int d=0; d<2; d++) { // dfs이니까 위에만 보면 된다.
-        int nr = r;
-        int nc = c;
-        while(1) {
-            nr += dr[d];
-            nc += dc[d];
-            if(inRange(nr,nc)) {
-                if(maps[nr][nc]) return false;
-            } else {
-                break;
-            }
-        }
-    }
-
+bool check(int r, int c) {
+    if(Rvis[r]) return false;
+    if(Cvis[c]) return false;
+    if(cross1[N+r-c]) return false;
+    if(cross2[r+c]) return false;
     return true;
 }
 
-void dfs(int r) { // 한 행에 한 퀸을 놓아야 N개를 놓을 수 있다.
+void dfs(int r) {
     if(r == N) {
         ans++;
         return;
     }
 
     for(int c=0; c<N; c++) {
-        if(!visCol[c] && checkDiag(r,c)) {
-            visCol[c] = true;
-            maps[r][c] = true;
-            
+        if(check(r,c)) {
+            vis(r,c,true);
             dfs(r+1);
-    
-            visCol[c] = false;
-            maps[r][c] = false;
+            vis(r,c,false);
         }
     }
-
-    
 }
 
 int main() {
-    cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
     cin >> N;
-    maps.resize(N, vector<bool>(N,false));
-    visCol.resize(N,false);
-    visRow.resize(N,false);
+    Rvis.resize(N, false);
+    Cvis.resize(N, false);
+    cross1.resize(N*2 + 1, false);
+    cross2.resize(N*2, false);
 
     dfs(0);
+    
     cout << ans;
-    return 0;
 }
