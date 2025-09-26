b5762809b6a6497dec55b562c7812e8d3ec827a4
Jiwoo <emily1290@naver.com>
2025-09-26 20:58:34 +0900

    [Gold IV] Title: 무기 공학, Time: 32 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/18430. 무기 공학/무기 공학.cc
+++ 백준/Gold/18430. 무기 공학/무기 공학.cc
@@ -2,76 +2,54 @@
 #include <vector>
 
 using namespace std;
-
-int N,M; int maxAns = 0;
+int N, M; int ans = 0;
 vector<vector<int>> maps;
-vector<vector<bool>> vis;
+vector<vector<bool>> dp;
 
-int dr[] = {-1,0, -1,0, 0,1, 0,1};
-int dc[] = {0,-1,  0,1, 1,0, -1,0};
-void dfs(int idx, int sum);
+int dr[] = {1, 0, -1, 0};
+int dc[] = {0, 1, 0, -1};
 
 bool inRange(int r, int c) {
     return r>=0 && r<N && c>=0 && c<M;
 }
+void dfs(int idx, int sum);
+void check(int r, int c, int idx, int d1, int d2, int sum) {
 
-void makeSum(int idx, int sum, int r,int c, int sd) {
-    bool possible = true;
-    int tmpSum = maps[r][c]*2;
-
-    for(int d=sd; d<sd+2; d++) {
-        int nr = r + dr[d];
-        int nc = c + dc[d];
-        if(!inRange(nr, nc) || vis[nr][nc]) {
-            possible = false;
-        } else {
-            tmpSum += maps[nr][nc];
-        }
+    int nr = r + dr[d1]; int nc = c + dc[d1];
+    int nr2 = r + dr[d2]; int nc2 = c+ dc[d2];
+    if(inRange(nr,nc) && inRange(nr2, nc2) && !dp[r][c] && !dp[nr][nc] && !dp[nr2][nc2]) {
+        dp[r][c] = dp[nr][nc] = dp[nr2][nc2] = true;
+        dfs(idx+1, sum+maps[r][c]*2 + maps[nr][nc] + maps[nr2][nc2]);
+        dp[r][c] = dp[nr][nc] = dp[nr2][nc2] = false;
     }
-
-    if(possible) {
-        vis[r][c] = vis[r+dr[sd]][c+dc[sd]] = vis[r+dr[sd+1]][c+dc[sd+1]] = true;
-        dfs(idx+1, sum+tmpSum);
-        vis[r][c] = vis[r+dr[sd]][c+dc[sd]] = vis[r+dr[sd+1]][c+dc[sd+1]] = false;
-    }
-    
 }
 
 void dfs(int idx, int sum) {
     if(idx == N*M) {
-        maxAns = max(maxAns, sum);
+        ans = max(ans, sum);
         return;
     }
 
     int r = idx/M;
     int c = idx%M;
 
-    if(!vis[r][c]) {
-        makeSum(idx,sum,r,c,0);
-        makeSum(idx,sum,r,c,2);
-        makeSum(idx,sum,r,c,4);
-        makeSum(idx,sum,r,c,6);
-    } 
-    
-    dfs(idx+1, sum); // 그냥 지나갈 경우
-    
-    
+    check(r,c,idx,0,3,sum);
+    check(r,c,idx,2,3,sum);
+    check(r,c,idx, 1,2,sum);
+    check(r,c,idx,0,1,sum);
+    dfs(idx+1, sum);
 }
 
 int main() {
-    cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
-    cin >> N >> M; // 세로 가로
+    cin >> N >> M;
     maps.resize(N, vector<int>(M, 0));
-    vis.resize(N, vector<bool>(M, false));
-
+    dp.resize(N, vector<bool>(M, false));
     for(int r=0; r<N; r++) {
         for(int c=0; c<M; c++) {
             cin >> maps[r][c];
         }
     }
-
-    dfs(0,0);
-    cout << maxAns;
     
-    return 0;
+    dfs(0, 0);
+    cout << ans;
 }
