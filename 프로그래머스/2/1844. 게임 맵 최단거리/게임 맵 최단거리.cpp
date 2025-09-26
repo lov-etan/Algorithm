f10faa15e143b8563b7ca43187783f25b7934a43
Jiwoo <emily1290@naver.com>
2025-09-27 00:05:49 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.32 ms, Memory: 4.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,12 +1,10 @@
 #include <vector>
 #include <queue>
-#include <iostream>
-
 using namespace std;
-
-int N; int M;
-vector<vector<int> > arr;
-vector<vector<int>> dist;
+int N, M; 
+vector<vector<int>> arr;
+vector<vector<int>> dp;
+queue<pair<int,int>> q;
 
 int dr[] = {1,0,-1,0};
 int dc[] = {0,1,0,-1};
@@ -14,49 +12,32 @@ int dc[] = {0,1,0,-1};
 bool inRange(int r, int c) {
     return r>=0 && r<N && c>=0 && c<M;
 }
- 
+
 int bfs() {
-    queue<pair<int,int>> q;
-    pair<int, int> start = {0,0};
-    q.push(start);
-    dist[0][0] = 1;
-    
-    
+    dp[0][0] = 1;
+    q.push({0,0});
     while(!q.empty()) {
-        pair<int, int> pos = q.front(); q.pop();
         
-        int r = pos.first;
-        int c = pos.second;
+        auto[r,c] = q.front(); q.pop();
         
-        // cout << r << " " << c << "\n";
-
         if(r == N-1 && c == M-1) {
-            return dist[r][c];
+            return dp[r][c];
         }
-
         for(int d=0; d<4; d++) {
-            int nr = r+dr[d];
-            int nc = c+dc[d];
-            if(inRange(nr,nc) && dist[nr][nc] == 0 && arr[nr][nc] == 1) {
-                dist[nr][nc] = dist[r][c]+1;
-                
-                pair<int, int> nxtPos = {nr, nc};
-                q.push(nxtPos);
+            int nr = r + dr[d]; int nc = c + dc[d];
+            if(inRange(nr,nc) && dp[nr][nc] == -1 && arr[nr][nc] == 1) {
+                dp[nr][nc] = dp[r][c]+1;
+                q.push({nr,nc});
             }
         }
     }
-    
     return -1;
-    
-    
 }
 
-int solution(vector<vector<int> > maps)
-{
-    int answer = 0;
-    arr = maps; N = maps.size(); M = maps[0].size();
-    dist.resize(N, vector<int>(M,0));
+int solution(vector<vector<int>> maps) {
+    N = maps.size(); M = maps[0].size();
+    arr = maps; dp.resize(N, vector(M, -1));
     
-    answer = bfs();
-    return answer;
+    
+    return bfs();
 }
