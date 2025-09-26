a0893d98972be0608e9961d3611507b6f8fb0596
Jiwoo <emily1290@naver.com>
2025-09-27 00:26:29 +0900

    [level 3] Title: 아이템 줍기, Time: 0.07 ms, Memory: 4.44 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.cpp
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.cpp
@@ -3,66 +3,69 @@
 #include <queue>
 
 using namespace std;
-int cX, cY, iX, iY;
-vector<vector<int>> maps(101, vector<int>(101,0));
-vector<vector<int>> dp(101, vector<int>(101,-1));
+vector<vector<int>> maps;
+vector<vector<int>> dp;
+int tr, tc;
 queue<pair<int,int>> q;
 
-int dr[] = {-1,0,1,0};
-int dc[] = {0, 1, 0, -1};
+int dr[] = {1,0,-1,0};
+int dc[] = {0,1,0,-1};
 
 bool inRange(int r, int c) {
     return r>=0 && r<101 && c>=0 && c<101;
 }
 
-int bfs() {
-    dp[cX][cY] = 0;
-    q.push({cX, cY});
+int bfs(int sr, int sc) {
+    dp[sr][sc] = 0;
+    q.push({sr,sc});
     
     while(!q.empty()) {
-        auto [r,c] = q.front(); q.pop();
-        if(r==iX && c==iY) {
-            return dp[r][c];
+        auto[r,c] = q.front(); q.pop();
+        if(r==tr && c==tc) {
+            return dp[r][c]/2;
         }
         
         for(int d=0; d<4; d++) {
-            int nr = r + dr[d];
-            int nc = c + dc[d];
-            if(inRange(nr,nc) && maps[nr][nc] == 1 && dp[nr][nc] == -1) {
-                dp[nr][nc] = dp[r][c] + 1;
+            int nr = r+dr[d]; int nc = c+dc[d];
+            if(inRange(nr,nc) && dp[nr][nc] == -1 && maps[nr][nc] == 1) {
+                dp[nr][nc] = dp[r][c]+1;
                 q.push({nr,nc});
             }
         }
     }
+    return -1;
 }
 
 int solution(vector<vector<int>> rectangle, int characterX, int characterY, int itemX, int itemY) {
-    int answer = 0;
-    cX = 2 * characterX; cY = 2 * characterY; iX = 2 * itemX; iY = 2 * itemY;
+    maps.resize(101, vector<int>(101,0));
+    dp.resize(101, vector<int>(101,-1));
+    tr = itemX*2; tc = itemY*2;
     
-    // 모두 1로 채워주기
     for(int i=0; i<rectangle.size(); i++) {
-        int r1 = rectangle[i][0] * 2; int c1 = rectangle[i][1] * 2; 
-        int r2 = rectangle[i][2] * 2; int c2 = rectangle[i][3] * 2;
+        int sr = rectangle[i][0]*2; int sc = rectangle[i][1]*2;
+        int er = rectangle[i][2]*2; int ec = rectangle[i][3]*2;
         
-        for(int r=r1; r<=r2; r++) {
-            for(int c=c1; c<=c2; c++) {
-                maps[r][c] = 1; 
+        for(int r=sr; r<=er; r++) {
+            maps[r][sc] = 1;
+            maps[r][ec] = 1;
+        }
+        
+        for(int c=sc; c<=ec; c++) {
+            maps[sr][c] = 1;
+            maps[er][c] = 1;
+        }
+    }
+    
+    for(int i=0; i<rectangle.size(); i++) {
+        int sr = rectangle[i][0]*2; int sc = rectangle[i][1]*2;
+        int er = rectangle[i][2]*2; int ec = rectangle[i][3]*2;
+        
+        for(int r=sr+1; r<er; r++) {
+            for(int c=sc+1; c<ec; c++) {
+                maps[r][c] = 0;
             }
         }
     }
     
-    // 테두리 안은 0으로 비워주기
-    for(int i=0; i<rectangle.size(); i++) {
-        int r1 = rectangle[i][0] * 2; int c1 = rectangle[i][1] * 2; 
-        int r2 = rectangle[i][2] * 2; int c2 = rectangle[i][3] * 2;
-        
-        for(int r=r1+1; r<=r2-1; r++) {
-            for(int c=c1+1; c<=c2-1; c++) {
-                maps[r][c] = 0; 
-            }
-        }
-    }
-    
-    return bfs()/2;
+    return bfs(characterX*2, characterY*2);
 }
