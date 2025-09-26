6c05919a4d75fe85fee42190d195f5ac8726d68a
Jiwoo <emily1290@naver.com>
2025-09-26 10:43:36 +0900

    [Gold V] Title: 선발 명단, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/3980. 선발 명단/선발 명단.cc
+++ 백준/Gold/3980. 선발 명단/선발 명단.cc
@@ -1,42 +1,43 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
+#include <algorithm>
 
 using namespace std;
-vector<vector<int>> powers;
+vector<vector<int>> arr;
 vector<bool> vis;
-int maxAns;
 
-void dfs(int sIdx, int sum) {
-    if(sIdx == 11) {
-        maxAns = max(maxAns, sum);
+int maxP = -1;
+
+void dfs(int ath, int sum) {
+    if(ath == 11) {
+        maxP = max(maxP, sum);
         return;
     }
 
-    for(int i=0; i<11; i++) {
-        if(!vis[i] && powers[i][sIdx] > 0) {
-            vis[i] = true;
-            dfs(sIdx+1, sum + powers[i][sIdx]);
-            vis[i] = false;
+    for(int p=0; p<11; p++) {
+        if(!vis[p] && arr[ath][p] > 0) {
+            vis[p] = true;
+            dfs(ath+1, sum + arr[ath][p]);
+            vis[p] = false;
         }
     }
 }
 
 int main() {
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
     int T; cin >> T;
     while(T--) {
-        powers.assign(11,vector<int>(11,0));
-        vis.assign(11,false);
-        maxAns = -1;
-
+        maxP = -1;
+        arr.assign(11, vector<int>(11,0));
+        vis.assign(11, false);
         for(int r=0; r<11; r++) {
             for(int c=0; c<11; c++) {
-                cin >> powers[r][c];
+                cin >> arr[r][c];
             }
         }
-
         dfs(0,0);
-        cout << maxAns << "\n";
+        cout << maxP << "\n";
     }
+       
     return 0;
 }
