89b814a9f7a696d7116a4ab5ccd14e8d10d9150a
Jiwoo <emily1290@naver.com>
2025-09-26 21:36:23 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,17 +1,14 @@
-# 가장 조회수가 높은 게시물?
-# 그 게시물의 첨부파일 경로!
- WITH 
- cte AS (
-    SELECT MAX(views) as max_view
-    FROM USED_GOODS_BOARD
- ), 
- cte2 AS (
+# 조회수 가장 높은 게시물? 
+# 에 대한 첨부파일 경로!
+WITH cte AS (
     SELECT board_id
     FROM USED_GOODS_BOARD
-    WHERE views = (SELECT max_view FROM cte)
- )
+    ORDER BY VIEWS DESC
+    LIMIT 1
+)
 
-SELECT concat('/home/grep/src/', board_id, '/', file_id, file_name, file_ext) as file_path
+# 기본적인 파일경로는 /home/grep/src/ 이며, 게시글 ID를 기준으로 디렉토리가 구분되고, 파일이름은 파일 ID, 파일 이름, 파일 확장자로 구성되도록 출력
+SELECT CONCAT('/home/grep/src/', c.board_id, '/', file_id, file_name, file_ext) as file_path
 FROM USED_GOODS_FILE as f
-WHERE board_id = (SELECT board_id FROM cte2)
+    JOIN cte as c ON f.board_id = c.board_id
 ORDER BY file_id DESC
