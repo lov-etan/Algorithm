6660c5a3bfee5a4f7edf08adcb37f16098f3d2ed
Jiwoo <emily1290@naver.com>
2025-11-05 10:37:08 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -2,26 +2,19 @@
 #include <vector>
 
 using namespace std;
-int INF = 2076543219;
+int INF = 98765432;
 
 int main() {
-    cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
     int N; cin >> N;
-    vector<int> dp(N+1, 0);
+    vector<int> dp(1000001, INF);
+
     dp[1] = 0; dp[2] = 1; dp[3] = 1;
     for(int i=4; i<=N; i++) {
-        int a = INF, b = INF, c = INF;
-                
-        if(i%3 == 0) a = dp[i/3] + 1;
-        if(i%2 == 0) b = dp[i/2] + 1;
-        c = dp[i-1]+1;
-
-        if(a <= b && a <= c) dp[i] = a;
-        else if(b <= a && b <= c) dp[i] = b;
-        else if(c <= a && c <= b) dp[i] = c;
+        if(i%3 == 0) dp[i] = dp[i/3]+1;
+        if(i%2 == 0) dp[i] = min(dp[i], dp[i/2]+1);
+        dp[i] = min(dp[i], dp[i-1]+1);
+        // cout << i << " " << dp[i] << "\n";
     }
-
     cout << dp[N];
-    
     return 0;
 }
