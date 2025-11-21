8e06fac901aa18a31e7fad36bd83e2c8224e61f1
Jiwoo <emily1290@naver.com>
2025-11-21 09:46:12 +0900

    [level 3] Title: 단어 변환, Time: 0.63 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/README.md
+++ 프로그래머스/3/43163. 단어 변환/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단어 변환 - 43163 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163?language=java) 
 
 ### 성능 요약
 
-메모리: 4.28 MB, 시간: 0.02 ms
+메모리: 86.7 MB, 시간: 0.63 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 26일 00:10:11
+2025년 11월 21일 09:46:09
 
 ### 문제 설명
 
--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,85 +1,59 @@
 import java.util.*;
 
-class Solution {
+class Node {
+    String w;
+    int cnt;
     
-    class Node{
-        String word;
-        int cnt;
+    Node(String w, int cnt) {
+        this.w = w;
+        this.cnt = cnt;
+    }
+}
 
-        Node(String word, int cnt){
-            this.word = word;
-            this.cnt = cnt;
+class Solution {
+    String target;
+    String[] words;
+    boolean[] vis;
+    
+    boolean check(String a, String b) {
+        int cnt = 0;
+        // 다른게 오직 하나!
+        for(int i=0; i<a.length(); i++) {
+            char chA = a.charAt(i); char chB = b.charAt(i);
+            if(chA != chB) cnt++;
         }
+        
+        return cnt == 1;
     }
     
-    public int solution(String begin, String target, String[] words) {
-        List<String> list = new ArrayList<>();
-        list.addAll(Arrays.asList(words)); //우와 이런 것도 있네
-        
-        boolean[] visited = new boolean[words.length];
-
-        int answer = Integer.MAX_VALUE ; //minCnt
-
-        /////////////////// bfs 시작////////////////////////////
+    int bfs(String start) {
         Queue<Node> q = new LinkedList<>();
-        q.add(new Node(begin, 0)); //hit 넣기
-
+        q.add(new Node(start, 0));
+        
         while(!q.isEmpty()) {
-            Node curr = q.poll(); //hit을 빼고
-            //curr.cnt++; // 이렇게 하니 틀렸어 
-            System.out.println(curr.word);
-            
-            if(curr.word.equals(target)){
-                answer = Math.min(answer, curr.cnt);
-                break;
+            Node curr = q.poll();
+            if(curr.w.equals(target)) {
+                return curr.cnt;
             }
             
-            
-            for(int k=0; k< words.length; k++){
-                if(!visited[k]){
-                 String next = words[k];
-                int same = 0;
-                for(int j=0; j<begin.length(); j++){
-                    if( curr.word.charAt(j) == next.charAt(j) ){
-                        same++;
-                    }
+            for(int i=0; i<words.length; i++) {
+                if(!vis[i] && check(curr.w, words[i])) {
+                    vis[i] = true;
+                    q.add(new Node(words[i], curr.cnt+1));
                 }
-                
-                if(same == begin.length()-1){
-                    visited[k] = true; 
-                    q.add(new Node(next, curr.cnt+1));
-                }   
-                }
-                
-            }//
-            
-            
-            // //다음 타깃을 찾는다.
-            // for(int k=list.size()-1; k>=0; k--){
-            // // list 중 단어 하나 뽑아
-            //     int same = 0;
-            //     String next = list.get(k);
-            //     // 한 글자만 달라야 해 
-            //     for(int j=0; j<begin.length(); j++){
-            //         if(curr.word.charAt(j) == next.charAt(j)){
-            //             same++;
-            //         }
-            //     }
-            //     //만약 한 글자만 다르면 ? - visited[](list에서 빼기) 처리 해주고 큐에 넣어주기 
-            //     if(same == begin.length()-1){
-            //         list.remove(k); //visited 처리 (다시는 돌아갈 일 없다 - list 안에서 찾는데 이걸 없앴으니까)
-            //         q.add(new Node(next, curr.cnt));
-            //     }
-            // }
-
+            }
         }
-        ///////////////////////////////////////////////////////
-
-
-        if(answer == Integer.MAX_VALUE) {
-            answer = 0;
-        }
-
-        return answer;
+        return 0;
+    }
+     
+    public int solution(String begin, String target, String[] words) {
+        this.target = target;
+        this.words = words;
+        
+        vis = new boolean[words.length];
+        return bfs(begin);
+        
+        // int answer = 0;
+        // return answer;
     }
 }
