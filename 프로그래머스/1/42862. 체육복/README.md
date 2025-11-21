037f02028b4b35529336e0d2c09793282258db76
Jiwoo <emily1290@naver.com>
2025-11-21 12:24:34 +0900

    [level 1] Title: 체육복, Time: 0.20 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862#qna) 
 
 ### 성능 요약
 
-메모리: 87.3 MB, 시간: 0.52 ms
+메모리: 82.3 MB, 시간: 0.20 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 04일 17:41:17
+2025년 11월 21일 12:24:32
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,64 +1,58 @@
 import java.util.*;
 class Solution {
+    boolean[] rVis;
+    int n;
+    int[] lost;
+    int[] reserve;
+    int answer = 0;
+    
+    void dfs(int sum, int idx) {
+        answer = Math.max(answer, sum);
+        if(idx == lost.length) {
+            return;
+        }
+        
+        int num = lost[idx];
+        for(int r=0; r<reserve.length; r++) {
+            if(rVis[r]) continue;
+            if(reserve[r] == num-1 || reserve[r] == num+1) {
+                rVis[r] = true;
+                dfs(sum+1, idx+1);
+                rVis[r] = false;
+            }
+        }
+        dfs(sum, idx+1);
+        
+    }
+    
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
-        // 지금까지 체육복을 갖고 있는 학생들 
-        int cnt = n - lost.length;
+        this.n = n;
         
-        Arrays.sort(lost);
-        Arrays.sort(reserve);
+        this.reserve = reserve;
+        rVis = new boolean[reserve.length];
         
-        // 학생들 체육복 체크 배열 (갖고있음-true / 안 갖고있음-false)
-        boolean[] students = new boolean[n+1];
-        for(int s = 1; s<=n; s++){
-            students[s] = true;
-            for(int l=0; l<lost.length; l++){
-                if(s == lost[l]){
-                    students[s] = false;
-                }
+        HashSet<Integer> lostSets = new HashSet<>();
+        for(int i=0; i<lost.length; i++) {
+            lostSets.add(lost[i]);
+        }
+        for(int i=0; i<reserve.length; i++) {
+            if(lostSets.contains(reserve[i])) {
+                rVis[i] = true;
+                lostSets.remove(reserve[i]);
             }
         }
-        
-        //내가 여분의 옷이 있는데 lost에 들었어. 내거 먼저 구하고 생각하자
-        for(int i=0; i<reserve.length; i++){
-            int giver = reserve[i];
-            for(int j=0; j<lost.length; j++){
-                int taker = lost[j];
-                if(giver == taker && !students[taker]){ 
-                    cnt++;
-                    students[taker] = true;
-                    //더이상 lost, reserve에 포함되지 않게 무의미한 값으로 바꾼다
-                    lost[j] = -1;
-                    reserve[i] = -1;
-                    break;
-                }
-            }
+        lost = new int[lostSets.size()];
+        int idx = 0;
+        for(int s : lostSets) {
+            lost[idx] = s;
+            idx++;
         }
         
-        // 체육복 분배 시작 
-        for(int i=0; i<reserve.length; i++){
-            int giver = reserve[i]; //여분의 체육복을 줄 사람의 번호 
-            //System.out.println("giver" + giver);
-            
-            for(int j=0; j<lost.length; j++){
-                int taker = lost[j]; //여분의 체육복을 받을 사람의 번호 
-                //System.out.println("taker" + taker);
-                
-                //2. 내 이전 번호가 lost라면?
-                if(giver!=1 && giver-1 == taker && !students[taker]){
-                    cnt++;
-                    students[taker] = true;
-                    break;
-                }
-                //3. 내 이후 번호가 lost라면?
-                if(giver!=n && giver+1 == taker && !students[taker]){
-                    cnt++; 
-                    students[taker] = true;
-                    break;
-                }
-            }
-        }
+        this.lost = lost;
+        int curr = n - lostSets.size();
         
-        return cnt;
+        dfs(curr, 0);
+        
+        return answer;
     }
 }
