8c3eceef38d1628663745eb0e7545a3b89df77e4
Jiwoo <emily1290@naver.com>
2025-11-21 09:24:37 +0900

    [level 3] Title: 네트워크, Time: 0.33 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java) 
 
 ### 성능 요약
 
-메모리: 4.14 MB, 시간: 0.08 ms
+메모리: 88.3 MB, 시간: 0.33 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 26일 23:51:40
+2025년 11월 21일 09:24:33
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,49 +1,43 @@
 import java.util.*;
 
 class Solution {
-    int[] p;
+    int n;
+    int[][] computers;
+    Queue<Integer> q = new LinkedList<>();
+    boolean vis[];
     
-    public int solution(int n, int[][] computers) {
-        //루트는 0~n-1 까지 있음
-        p = new int[n];
-        for(int x=0; x<n ; x++){
-            p[x] = x; //초기에는 자기 자신이 자기 자신의 부모 노드 
-        }
+    void bfs(int start) {
+        vis[start] = true;
+        q.add(start);
         
-        boolean[][] visited = new boolean[n][n];
-        
-        //인접행렬 돌면서 연결되어 있는 것들 같은 집합에 넣어주기 
-        for(int root = 0; root < n; root++){
-            for(int other=0; other<n; other++){
-                if(!visited[root][other] && computers[root][other] == 1){
-                    unionSet(root, other);
-                    visited[root][other] = true;
-                    visited[other][root] = true;
+        while(!q.isEmpty()) {
+            int curr = q.poll();
+            
+            for(int i=0; i<n; i++) {
+                if(computers[curr][i] == 1 && !vis[i]) {
+                    vis[i] = true;
+                    q.add(i);
                 }
             }
         }
         
-        int answer = 0;
         
-        //이제 p[]에 각 부모 노드가 저장되었겠지?
-        Set<Integer> set = new LinkedHashSet<>();
-        for(int i=0; i<n; i++){
-            set.add(findSet(p[i])) ;
+    }
+    
+    public int solution(int n, int[][] computers) {
+        this.n = n;
+        this.computers = computers;
+        
+        int answer = 0;
+        vis = new boolean[n];
+        for(int i=0; i<n ;i++) {
+            if(!vis[i]) {
+                bfs(i);
+                answer++;
+            }
         }
         
-        answer = set.size();
         
         return answer;
-    }
-    
-    void unionSet(int x, int y){
-        p[findSet(y)] = findSet(x);
-    }
-    
-    int findSet(int x){
-        if(x != p[x]){
-            p[x] = findSet(p[x]);
-        }
-        return p[x];
     }
 }
