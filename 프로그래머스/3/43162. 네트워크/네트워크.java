60627fa53aa0aa64e7da103344c01cb4f96609aa
Jiwoo <emily1290@naver.com>
2025-11-22 09:19:21 +0900

    [level 3] Title: 네트워크, Time: 0.30 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -3,35 +3,35 @@ import java.util.*;
 class Solution {
     int n;
     int[][] computers;
-    Queue<Integer> q = new LinkedList<>();
-    boolean vis[];
+    boolean[] vis;
     
     void bfs(int start) {
-        vis[start] = true;
+        Queue<Integer> q = new LinkedList<>();
         q.add(start);
         
         while(!q.isEmpty()) {
             int curr = q.poll();
             
             for(int i=0; i<n; i++) {
-                if(computers[curr][i] == 1 && !vis[i]) {
+                if(i == curr) continue;
+                if(!vis[i] && computers[curr][i] == 1) {
                     vis[i] = true;
                     q.add(i);
                 }
             }
         }
-        
-        
     }
     
     public int solution(int n, int[][] computers) {
         this.n = n;
         this.computers = computers;
+        vis = new boolean[n];
         
         int answer = 0;
-        vis = new boolean[n];
-        for(int i=0; i<n ;i++) {
+        
+        for(int i=0; i<n; i++) {
             if(!vis[i]) {
+                vis[i] = true;
                 bfs(i);
                 answer++;
             }
