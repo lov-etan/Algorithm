3636cb33510b1c4b6af885315f22d455b5ed9e83
Jiwoo <emily1290@naver.com>
2025-11-22 09:31:39 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.91 ms, Memory: 54.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,37 +1,38 @@
 import java.util.*;
 
 class Solution {
+    int N, M;
     int[][] maps;
-    int N; int M;
     boolean[][] vis;
+    Queue<int[]> q = new LinkedList<>();
     
     int dr[] = {1,0,-1,0};
     int dc[] = {0,1,0,-1};
     
-    boolean inRange(int r, int c) {
+    boolean inRange(int r,int c) {
         return r>=0 && r<N && c>=0 && c<M;
     }
     
-    int bfs(int sr, int sc) {
-        Queue<int[]> q = new LinkedList<>();
-        vis[sr][sc] = true;
-        q.add(new int[]{sr,sc,1});
+    int bfs() {
+        vis[0][0] = true;
+        q.add(new int[]{0,0,1});
         
         while(!q.isEmpty()) {
             int[] curr = q.poll();
             int r = curr[0]; int c = curr[1]; int cnt = curr[2];
-            if(r == N-1 && c==M-1) return cnt;
+            if(r==N-1 && c==M-1) {
+                return cnt;
+            }
             
             for(int d=0; d<4; d++) {
-                int nr = r+dr[d];
-                int nc = c+dc[d];
-                
-                if(inRange(nr,nc) && maps[nr][nc] == 1 && !vis[nr][nc]) {
+                int nr =r+dr[d]; int nc = c+dc[d];
+                if(inRange(nr,nc) && !vis[nr][nc] && maps[nr][nc] == 1) {
                     vis[nr][nc] = true;
                     q.add(new int[]{nr,nc,cnt+1});
                 }
             }
         }
+        
         return -1;
     }
     
@@ -40,7 +41,6 @@ class Solution {
         N = maps.length; M = maps[0].length;
         vis = new boolean[N][M];
         
-        return bfs(0,0);
-        
+        return bfs();
     }
 }
