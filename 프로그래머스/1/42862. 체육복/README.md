127fb25d901bb7a4f8d2f26e197562d3d2d964ab
Jiwoo <emily1290@naver.com>
2025-11-25 08:52:00 +0900

    [level 1] Title: 체육복, Time: 0.06 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=java) 
 
 ### 성능 요약
 
-메모리: 82.3 MB, 시간: 0.20 ms
+메모리: 89.4 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 21일 12:24:32
+2025년 11월 25일 08:51:55
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,57 +1,55 @@
 import java.util.*;
+
 class Solution {
-    boolean[] rVis;
     int n;
-    int[] lost;
-    int[] reserve;
+    boolean[] lostVis;
+    boolean[] reserveVis;
     int answer = 0;
     
-    void dfs(int sum, int idx) {
-        answer = Math.max(answer, sum);
-        if(idx == lost.length) {
+    void dfs(int idx, int cnt) {
+        if(idx == n+1) {
+            answer = Math.max(answer, cnt);
             return;
         }
         
-        int num = lost[idx];
-        for(int r=0; r<reserve.length; r++) {
-            if(rVis[r]) continue;
-            if(reserve[r] == num-1 || reserve[r] == num+1) {
-                rVis[r] = true;
-                dfs(sum+1, idx+1);
-                rVis[r] = false;
+        if(lostVis[idx]) {
+            if(idx >= 2 && reserveVis[idx-1]) {
+                lostVis[idx] = false; reserveVis[idx-1] = false;
+                dfs(idx+1, cnt+1);
+                lostVis[idx] = true; reserveVis[idx-1] = true;
+            }
+            if(idx <= n-1 && reserveVis[idx+1]) {
+                lostVis[idx] = false; reserveVis[idx+1] = false;
+                dfs(idx+1, cnt+1);
+                lostVis[idx] = true; reserveVis[idx+1] = true;
             }
         }
-        dfs(sum, idx+1);
         
+        dfs(idx+1, cnt);
     }
     
     public int solution(int n, int[] lost, int[] reserve) {
         this.n = n;
+        lostVis = new boolean[n+1];
+        reserveVis = new boolean[n+1];
         
-        this.reserve = reserve;
-        rVis = new boolean[reserve.length];
-        
-        HashSet<Integer> lostSets = new HashSet<>();
-        for(int i=0; i<lost.length; i++) {
-            lostSets.add(lost[i]);
+        int lostCnt = lost.length;
+        for(int i=0; i<lost.length ;i++) {
+            lostVis[lost[i]] = true;
         }
-        for(int i=0; i<reserve.length; i++) {
-            if(lostSets.contains(reserve[i])) {
-                rVis[i] = true;
-                lostSets.remove(reserve[i]);
+        for(int i=0; i<reserve.length ;i++) {
+            reserveVis[reserve[i]] = true;
+        }
+        
+        for(int i=1; i<=n; i++) {
+            if(reserveVis[i] && lostVis[i]) {
+                lostCnt--;
+                reserveVis[i] = false;
+                lostVis[i] = false;
             }
         }
-        lost = new int[lostSets.size()];
-        int idx = 0;
-        for(int s : lostSets) {
-            lost[idx] = s;
-            idx++;
-        }
         
-        this.lost = lost;
-        int curr = n - lostSets.size();
-        
-        dfs(curr, 0);
+        dfs(1, n-lostCnt);
         
         return answer;
     }
