ba716d0c5d1d88c7f5427d7a5a00406b06943ce9
Jiwoo <emily1290@naver.com>
2025-11-27 00:42:13 +0900

    [level 2] Title: 구명보트, Time: 12.84 ms, Memory: 57.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,18 +1,26 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
-        Arrays.sort(people);
-        int left = 0;
-        int right = people.length-1;
         
-        while(left<=right){
-            if(people[left] + people[right] <= limit){
-                left++;
-            }
+        boolean[] vis = new boolean[people.length];
+        
+        int idx=0;
+        Arrays.sort(people);
+        for(int i=people.length-1; i>=0; i--) {
+            if(vis[i]) continue;
             
-            answer++;
-            right--;
+            int a = people[i]; int b = people[idx];
+            if(a+b <= limit) {
+                vis[i] = true; vis[idx] = true;
+                answer++;
+                idx++;
+            }
+        }
+        
+        for(int i=0; i<people.length; i++) {
+            if(!vis[i]) answer++;
         }
         
         return answer;
